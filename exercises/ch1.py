f1b2053878fa343be3d57ddf7c5db5152b3a740d
eembees <eembees@gmail.com>
2020-04-20 10:45:10 +0200

    first commit

---
+++ exercises/ch1.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+# Perform the computer simulation of 1-dimensional random walk on a lattice, with ∆x = 1 and
+# ∆t = 1. confirm that the mean and variance obeys what we have seen.
+import numpy as np
