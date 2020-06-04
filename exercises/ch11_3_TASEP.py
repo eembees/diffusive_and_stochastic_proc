5e819919b0661d79ab6f43bc361894c8802c31ed
eembees <eembees@gmail.com>
2020-06-04 13:45:42 +0200

    worked more added flux

--- exercises/ch11_3_TASEP.py
+++ exercises/ch11_3_TASEP.py
@@ -7,6 +7,7 @@ from typing import List, Dict, Union, Tuple
 import pandas as pd
 import numpy as np
 from matplotlib import rcParams
+from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
 
 rcParams["font.family"] = "monospace"
 from matplotlib import pyplot as plt
@@ -14,7 +15,18 @@ import argparse
 import scipy.stats
 
 
-def main(args: Dict):
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+def main(args: Dict,):
     print("Running with values: ")
 
     print(
@@ -26,6 +38,7 @@ def main(args: Dict):
     const_L = args["length"]
     const_dT = args["timestep"]
     const_Tmax = args["maxtime"]
+    const_density = args["density"]
 
     const_nSteps = int(const_Tmax // const_dT)
     const_stepProb = const_dT * args["stepprob"]
@@ -37,7 +50,10 @@ def main(args: Dict):
     )  # now we access it as [time, position]
 
     # Populate the array at time 0
-    position_time_array[0] = np.random.binomial(n=1, p=args["density"], size=const_L)
+    if args["initrandom"]:  # Randomly populate
+        position_time_array[0] = np.random.binomial(n=1, p=const_density, size=const_L)
+    else:
+        position_time_array[0][: int(const_L * const_density)] = 1
 
     for i in range(1, const_nSteps):
         N_curr = np.copy(position_time_array[i - 1])
@@ -55,26 +71,65 @@ def main(args: Dict):
 
         position_time_array[i] = N_curr
 
-    # print(position_time_array)
+    # Calculate flux between timesteps
+    fluxmat = np.diff(position_time_array, axis=0)
+    fluxmat[fluxmat < 0] = 0
+    J = fluxmat.sum(axis=1)
+
+    if args["initrandom"]:  # Randomly populated means steady state more or less at once
+        Jmean = J.mean()
+        Jstd = J.std()
+    else:  # only use last 20% to guess SS
+        Jmean = J[-int(len(J) / 5.0) :].mean()
+        Jstd = J[-int(len(J) / 5.0) :].std()
+
+    J_theoretical = const_stepProb * const_density * (1 - const_density)
 
     (timepoints, positions) = np.nonzero(position_time_array)
 
-    df = pd.DataFrame({"Time": timepoints, "Pos": positions})
-
-    ax: plt.Axes
+    ax_traj: plt.Axes
     fig: plt.Figure
-    fig, ax = plt.subplots()
-    ax.set_title(
-        rf"Simulation with $L={const_L}, p\Delta t = {const_stepProb}, \rho={args['density']:.2f}$"
+
+    fig = plt.Figure(figsize=(7, 8))
+    gs = GridSpec(nrows=3, ncols=1, figure=fig)
+    ax_traj = fig.add_subplot(gs[:-1])
+    ax_flux = fig.add_subplot(gs[-1])
+
+    ax_flux.plot(J, alpha=0.7, lw=1)
+    ax_flux.axhline(
+        Jmean,
+        ls="-.",
+        alpha=0.5,
+        color="xkcd:pastel red",
+        label=rf"SS at $\operatorname{{E}}[J] = {Jmean:.2f}$",
+    )
+    ax_flux.axhspan(
+        Jmean - Jstd / 2, Jmean + Jstd / 2, alpha=0.2, color="xkcd:pastel orange",
     )
 
-    ax.scatter(positions, timepoints, marker=">", c="k", s=4)
+    ax_flux.axhline(
+        J_theoretical * const_L,
+        ls="--",
+        c="g",
+        label=rf"$J_{{theoretical}} \cdot L = {J_theoretical*const_L:.2f} $",
+    )
+
+    ax_flux.set_xlabel("Time")
+    ax_flux.set_ylabel(r"# particles moved ($J_{empirical}/L$) ")
+    ax_flux.legend(loc="upper right")
+
+    ax_traj.set_title(
+        rf"Simulation with $L={const_L}, p\Delta t = {const_stepProb}, \rho={const_density:.2f}$"
+    )
+
+    ax_traj.scatter(positions, timepoints, marker=">", c="k", s=4)
 
     if const_dT != 1:
-        ax.set_yticklabels([f"{v* const_dT:.2f}s" for v in ax.get_yticks()])
+        ax_traj.set_yticklabels([f"{v* const_dT:.2f}s" for v in ax_traj.get_yticks()])
+        ax_flux.set_xticklabels([f"{v* const_dT:.2f}s" for v in ax_flux.get_xticks()])
 
-    ax.set_xlabel("Position")
-    ax.set_ylabel("Time")
+    ax_traj.set_xlabel("Position")
+    ax_traj.set_ylabel("Time")
 
     fig.tight_layout()
 
@@ -91,20 +146,31 @@ def parse_arguments() -> Dict[str, Union[int, float, str, Path]]:
     parser.add_argument("-l", "--length", type=int, default=100)
     parser.add_argument("-dt", "--timestep", type=float, default=1.0)
     parser.add_argument("-p", "--stepprob", type=float, default=0.5)
-    parser.add_argument("-t", "--maxtime", type=int, default=100)
+    parser.add_argument("-t", "--maxtime", type=int, default=1000)
     parser.add_argument("-f", "--density", type=float, default=0.1)
+    parser.add_argument(
+        "-i",
+        "--initrandom",
+        type=str2bool,
+        nargs="?",
+        const=True,
+        default=False,
+        help="Initialize randomly.",
+    )
 
     parser.add_argument("-o", "--outdir", type=str, default="./figs")
     parser.add_argument("--filetype", type=str, choices=["png", "pdf"], default="pdf")
 
-    parser.add_argument("--TEST", type=bool, default=False)  # change to false once done
+    parser.add_argument(
+        "--TEST", type=str2bool, nargs="?", const=True, default=False, help="Test Mode."
+    )
 
     args = parser.parse_args()
     argdict = vars(args)  # returns a dict, easier to deal with
 
     if argdict["TEST"] == True:
         argdict["length"] = 15
-        argdict["maxtime"] = 50
+        argdict["maxtime"] = 20
         argdict["timestep"] = 1
 
     po = Path(argdict["outdir"])
--- exercises/ch11_3_TASEP_allrhos.sh
+++ exercises/ch11_3_TASEP_allrhos.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 for i in $(seq 0.1 0.1 0.9)
   do
-    python ch11_3_TASEP.py --density $i
+    python ch11_3_TASEP.py --density $i --filetype png --initrandom
   done
